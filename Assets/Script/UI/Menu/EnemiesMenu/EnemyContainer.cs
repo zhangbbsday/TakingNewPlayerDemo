a5030666c4ca51e46fac831bd82d3c2f29e3c589
zhangbbsday <1291238048@qq.com>
2020-09-22 22:51:11 +0800

    清理一遍命名空间。

--- Assets/Script/UI/Menu/EnemiesMenu/EnemyContainer.cs
+++ Assets/Script/UI/Menu/EnemiesMenu/EnemyContainer.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
+
 
 namespace GameEditor
 {
--- Assets/Script/UI/NoTextButtonEffect.cs
+++ Assets/Script/UI/NoTextButtonEffect.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace GameEditor
+﻿namespace GameEditor
 {
     public class NoTextButtonEffect : IButtonEffect
     {
